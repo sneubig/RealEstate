2113bd4c43db62b4b400a15ffedaac5b77de0c93
sneubig <neubig.stephen@gmail.com>
2015-08-06 19:50:10 -0400

    setup node, basic model structure with mongoose, and setup get post delete routes

---
+++ server.js
@@ -0,0 +1,77 @@
+// set up ========================
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+    // configuration =================
+
+    mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+
+    // define model ==================
+    var Listing = mongoose.model('Listing', {
+        price : 500
+    }
+
+    // Routes ===========
+
+    app.get('/api/listings', function(req, res) {
+        Listing.find(function(err, listings) {
+            if(err)
+                res.send(err)
+            res.json(listings);
+        });
+    });
+
+    app.post('/api/listings', function(req, res) {
+
+        Listing.create({
+            price : req.body.price, 
+            done : false
+        }, function(err, listing){
+            if(err)
+                res.send(err)
+
+            Listing.find(function(err, listings) {
+                if (err) {
+                    res.send(err)
+                res.json(listings);
+            });
+        });
+    });
+
+    app.delete('/api/listings/:listing_id', function(req, res){
+        Listing.remove({
+            _id : req.params.listing_id
+        }, function(err, listing){
+            if(err)
+                res.send(err);
+
+            Listing.find(function(err, listings){
+                if(err)
+                    res.send(err)
+                res.json(listings);
+            });
+        });
+    });
+
+
+    // application ----------------------
+
+    app.get('*', function(req, res){
+        res.sendfile('./public/index.html');
+    });
+
+    // listen (start app with node server.js) ======================================
+    app.listen(8080);
+    console.log("App listening on port 8080");
